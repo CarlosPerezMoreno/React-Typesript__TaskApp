bc3104de105afb31a660fb81cad7cfcfd399cd2f
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-15 20:50:36 +0200

    feat: Enviroment set up. Dependencies installed. Typescript configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import "./App.css";
 
-function App() {
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFontElement>) => {
+    event.preventDefault();
+    console.log("sending");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
+        <button>Save</button>
+      </form>
+    </Fragment>
   );
 }
 
