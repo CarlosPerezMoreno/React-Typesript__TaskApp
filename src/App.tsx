216c7ad3d6c155d40b1993e4b8a7e4d33941eb6a
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-16 11:17:04 +0200

    feat: Added creating tasks element. Bootstrap installed
    the first task element has been created with submit form. Styles improved with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-function App(): JSX.Element {
-  const [newTask, setNewTask] = useState("");
+type FormElement = React.FormEvent<HTMLFormElement>;
+interface TaskElement {
+  name: string;
+  done: boolean;
+}
 
-  const handleSubmit = (event: React.FormEvent<HTMLFontElement>) => {
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState<string>("");
+  const [taskList, setTaskList] = useState<TaskElement[]>([]);
+
+  const handleSubmit = (event: FormElement) => {
     event.preventDefault();
-    console.log("sending");
+    addTask(newTask);
+    console.log(taskList);
+  };
+
+  const addTask = (name: string) => {
+    const addingTask: TaskElement[] = [...taskList, { name, done: false }];
+    setTaskList(addingTask);
+    setNewTask("");
   };
 
   return (
-    <Fragment>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={(e) => setNewTask(e.target.value)} />
-        <button>Save</button>
-      </form>
-    </Fragment>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input
+                  className="form-control"
+                  type="text"
+                  onChange={(e) => setNewTask(e.target.value)}
+                  value={newTask}
+                  autoFocus
+                />
+                <button className="btn btn-success btn-block mt-2">Save</button>
+              </form>
+            </div>
+          </div>
+          {taskList.map((task: TaskElement, i: number) => (
+            <div className="card card-body mt-2" key={i}>
+              <h2 style={{ textDecoration: "line-through" }}>{task.name}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
